b31fca756411da7356b3033eb373ebf3bdbe74ca
Brandon Presley <brandonp2412@gmail.com>
2017-08-25 08:15:54 +1200

    Added programmatic method for valuing relics
    This is a new feature for building these tables in the Google sheets script editor.

--- valueRelics.js
+++ valueRelics.js
@@ -4,10 +4,122 @@
  * GitHub: https://github.com/brandonp2412/RelicValuation
  */
 
-function testValueItem() {
-  valueItem("Nikana Prime Hilt");
+function getDate() {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yyyy = today.getFullYear();
+  if(dd<10) {
+    dd = '0'+dd
+  } 
+  if(mm<10) {
+    mm = '0'+mm
+  } 
+  today = mm + '/' + dd + '/' + yyyy;
+  return today;
 }
 
+function test() {
+  filter('All', 'Testing', 'Axi');
+}
+
+// Takes all the records from All and separates them by relic
+function separate() {
+  filter('All', 'Axi', 'Axi');
+  filter('All', 'Neo', 'Neo');
+  filter('All', 'Meso', 'Meso');
+  filter('All', 'Lith', 'Lith');
+}
+
+function valueItem(item) {
+  var orders = figureItemType(item);
+  var maxBuy = getMaxBuy(orders.buy);
+  var minSell = getMinSell(orders.sell);
+  var today = new Date();
+  return [[minSell, maxBuy, getDate()]];
+}
+
+// Option for building All sheet programmatically
+function buildAll() {
+  var itemNames = getItemNames('All');
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var all = ss.getSheetByName('All');
+  var colCDE = all.getRange("C2:E");
+  for (var i = 1; i <= itemNames.length; i++) {
+    var item = itemNames[i-1];
+    try {
+      var value = valueItem(item);
+    }
+    catch (e) {
+      Logger.log(e);
+    }
+    colCDE.getCell(i, 1).setValue(value[0][0]);
+    colCDE.getCell(i, 2).setValue(value[0][1]);
+    colCDE.getCell(i, 3).setValue(value[0][2]);
+  }
+}
+
+/*
+ * Description: Filters rows from input table to output table based on whether 
+ *              or not they contain a pattern.
+ * @param {String} input    Table name of source data.
+ * @param {String} outut    Table name for output.
+ * @param {String} pattern  String to evaluate against each record of input.
+ */
+function filter(input, output, pattern) {
+  var inputTable = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(input);
+  var inputData = inputTable.getRange("A2:E").getDisplayValues();
+  var outputTable = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(output);
+  outputTable = outputTable.getRange("A2:E");
+  var offset = 0;
+  for (var i = 0; i < inputData.length; i++) {
+    if (!inputData[i][0].toString()) break;
+    if (inputData[i][0].toString().indexOf(pattern) !== -1) {
+      for (var j = 0; j < inputData[0].length; j++) {
+        outputTable.getCell(i+1-offset, j+1).setValue(inputData[i][j]);
+      }
+    } else {
+      offset++;
+    }
+  }
+}
+
+function getItemNames(sheet) {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var testing = ss.getSheetByName(sheet);
+  var colB = testing.getRange("B2:B").getDisplayValues();
+  var itemNames = [];
+  for (var i = 0; i < colB.length; i++) {
+    if (colB[i][0]) {
+      itemNames.push(colB[i]);
+    }
+  }
+  return itemNames;
+}
+
+function getRelicNames(sheet) {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var testing = ss.getSheetByName(sheet);
+  var colB = testing.getRange("A2:A").getDisplayValues();
+  var relicNames = [];
+  for (var i = 0; i < colB.length; i++) {
+    if (colB[i][0]) {
+      relicNames.push(colB[i]);
+    }
+  }
+  return relicNames;
+}
+
+function removeRows() {
+  var relicNames = getRelicNames("Axi");
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var axi = ss.getSheetByName("Axi");
+  for (var i = 0; i < relicNames.length; i++) {
+    if (relicNames[i].toString().indexOf("Neo") !== -1) {
+      axi.deleteRow(i+1);
+    }
+  }
+}
 
 // Items can be blueprints or sets. More often than not, they're blueprints.
 // But if they are actually a set this will change the URI to be correct.
@@ -25,12 +137,7 @@ function getOrders(item, url) {
   return JSON.parse(get).response;
 }
 
-function valueItem(item) {
-  var orders = figureItemType(item);
-  var maxBuy = getMaxBuy(orders.buy);
-  var minSell = getMinSell(orders.sell);
-  return [[minSell, maxBuy]];
-}
+
 
 function getMaxBuy(orders) {
   var buyPrices = [];
