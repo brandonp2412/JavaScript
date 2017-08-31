576c6b23f03a6cdc153c5fda31dbfa25617bf4de
unknown <brandonp2412@gmail.com>
2017-08-31 14:30:39 +1200

    Removed figureItem lingerings from master

--- valueRelics.js
+++ valueRelics.js
@@ -4,94 +4,26 @@
  * GitHub: https://github.com/brandonp2412/RelicValuation
  */
 
-function setupSpreadsheet() {
-  buildAll();
-  separate();
-  sortSheets();
-}
-
-function sortSheets() {
-  var sheetNames = ["All", "Axi", "Neo", "Meso", "Lith"];
-  for (var i = 0; i < sheetNames.length; i++) {
-    var ss = SpreadsheetApp.getActiveSpreadsheet();
-    var all = ss.getSheetByName(sheetNames[i]);
-    var data = all.getRange("A:E")
-    data.sort({column: 3, ascending: false})
-  } 
-}
-
-// Takes all the records from All and separates them by relic
-function separate() {
-  filter('All', 'Axi', 'Axi');
-  filter('All', 'Neo', 'Neo');
-  filter('All', 'Meso', 'Meso');
-  filter('All', 'Lith', 'Lith');
+function test() {
+  var minSell, maxBuy, date = valueItem("Tesla Link", "Mod");
+  Logger.log(minSell);
+  Logger.log(maxBuy);
+  Logger.log(date);
 }
 
 
-
-function valueItem(item) {
-  var orders = figureItemType(item);
+function valueItem(item, category) {
+  var orders = getOrders(item, category);
   var maxBuy = getMaxBuy(orders.buy);
   var minSell = getMinSell(orders.sell);
   var today = new Date();
   return [[minSell, maxBuy, getDate()]];
 }
 
-// Option for building All sheet programmatically
-function buildAll() {
-  var itemNames = getItemNames('All');
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
-  var all = ss.getSheetByName('All');
-  var colCDE = all.getRange("C2:E");
-  var items = [];
-  for (var i = 1; i <= itemNames.length; i++) {
-    var item = itemNames[i-1];
-    var found = findItem(items, item);
-    if (!found) {
-      try {
-        found = valueItem(item);
-        items.push({key: item, value: found});
-      }
-      catch (e) {
-        Logger.log(e);
-      }
-    }
-    colCDE.getCell(i, 1).setValue(found[0][0]);
-    colCDE.getCell(i, 2).setValue(found[0][1]);
-    colCDE.getCell(i, 3).setValue(found[0][2]);
-  }
-}
-
 function findItem(items, key) {
   for (var i = 0; i < items.length; i++) {
     if (items[i].key === key) {
       return items[i].value;
-    }
-  }
-}
-
-/*
- * Description: Filters rows from input table to output table based on whether 
- *              or not they contain a pattern.
- * @param {String} input    Table name of source data.
- * @param {String} outut    Table name for output.
- * @param {String} pattern  String to evaluate against each record of input.
- */
-function filter(input, output, pattern) {
-  var inputTable = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(input);
-  var inputData = inputTable.getRange("A2:E").getDisplayValues();
-  var outputTable = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(output);
-  outputTable = outputTable.getRange("A2:E");
-  var offset = 0;
-  for (var i = 0; i < inputData.length; i++) {
-    if (!inputData[i][0].toString()) break;
-    if (inputData[i][0].toString().indexOf(pattern) !== -1) {
-      for (var j = 0; j < inputData[0].length; j++) {
-        outputTable.getCell(i+1-offset, j+1).setValue(inputData[i][j]);
-      }
-    } else {
-      offset++;
     }
   }
 }
@@ -128,27 +60,16 @@ function removeRows() {
   var axi = ss.getSheetByName("Axi");
   for (var i = 0; i < relicNames.length; i++) {
     if (relicNames[i].toString().indexOf("Neo") !== -1) {
-      axi.deleteRow(i+1);
+      axi.devareRow(i+1);
     }
   }
 }
 
-// Items can be blueprints or sets. More often than not, they're blueprints.
-// But if they are actually a set this will change the URI to be correct.
-function figureItemType(item) {
-  var orders = getOrders(item, 'https://warframe.market/api/get_orders/Blueprint/');
-  if (orders === "Wrong request") {
-    orders = getOrders(item, 'https://warframe.market/api/get_orders/Set/');
-  }
-  return orders;
-}
-
-function getOrders(item, url) {
-  var url = encodeURI(url + item);
+function getOrders(item, category) {
+  var url = encodeURI("https://warframe.market/api/get_orders/" + category + "/" + item);
   var get = UrlFetchApp.fetch(url).getContentText();
   return JSON.parse(get).response;
 }
-
 
 
 function getMaxBuy(orders) {
