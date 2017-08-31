8d38405a659aef8a01508e43f70afeb2de320f25
unknown <brandonp2412@gmail.com>
2017-08-31 14:26:11 +1200

    Removed accidental includes

--- discord.js
+++
@@ -1,4 +0,0 @@
-def function hideSidebar() {
-	var sidebar = document.getElementsByClassName("channel-members-wrap")[0];
-	sidebar.parentNode.removeChild(sidebar)
-}
--- reddit.js
+++
@@ -1,5 +0,0 @@
-function hideSidebar() {
-	var sidebar = document.getElementsByClassName("side")[0];
-	sidebar.parentNode.removeChild(sidebar)
-}
-hideSidebar();
